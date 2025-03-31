6d2f030f9b110d759b83ab4a1d4813ace03d3ceb
Arthur <carvalhocostarthur@gmail.com>
2025-03-31 10:59:13 -0300

    Adição da API de livros

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,7 +1,19 @@
 import S from "./doados.module.scss";
 import livro from "../../assets/livroDoado.png";
+import axios from "axios"
+import {useState,useEffect} from 'react'
 
 export default function Doados() {
+  const [livros, setLivros] = useState([])
+  const getLivros = async() =>{
+    const response = await axios.get("https://api-livros-j05q.onrender.com/livros")
+    setLivros(response.data)
+    console.log(response)
+  }
+
+  useEffect(()=>{
+    getLivros()
+  },[])
   return (
     <section className={S.boxDoados}>
       <h2>Livros Doados</h2>
@@ -12,6 +24,14 @@ export default function Doados() {
           <p>Susanne Andrade</p>
           <p>Ficção</p>
         </article>
+        {livros.map((item)=>(
+          <article key={item.id}>
+          <img src={item.imagem_url} alt="" />
+          <h3>{item.titulo}</h3>
+          <p>{item.categoria}</p>
+          <p>{item.autor}</p>
+          </article>
+        ))}
       </section>
     </section>
   );
--- src/Pages/QueroDoar/querodoar.jsx
+++ src/Pages/QueroDoar/querodoar.jsx
@@ -1,7 +1,49 @@
 import S from "./querodoar.module.scss";
 import livro from "../../assets/livro.png";
+import axios from "axios"
+import {useState} from 'react'
 
 export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("")
+  const [categoria, setCategoria] = useState("")
+  const [autor, setAutor] = useState("")
+  const [imagem_url, setImagem_url] = useState("")
+
+  const enviarDados = async()=>{
+
+    const urlApi = "https://api-livros-j05q.onrender.com/doar"
+    const dadosEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url
+    }
+    await axios.post(urlApi,dadosEnviar)
+    alert('Livro enviado!')
+
+
+    setTitulo("")
+    setCategoria("")
+    setAutor("")
+    setImagem_url("")
+  }
+
+
+
+
+  const capturaTitulo = (e) =>{
+    setTitulo(e.target.value)
+  }
+  const capturaCategoria = (e) =>{
+    setCategoria(e.target.value)
+  }
+  const capturaAutor = (e) =>{
+    setAutor(e.target.value)
+  }
+  const capturaImagem = (e) =>{
+    setImagem_url(e.target.value)
+  }
+  
   return (
     <section className={S.principal}>
       <section className={S.container}>
@@ -9,21 +51,21 @@ export default function QueroDoar() {
           Por favor, preencha o formulário com suas informações e as informações
           do Livro
         </h2>
-        <form action="" className={S.formsBox}>
+        <form action="" className={S.formsBox} onSubmit={(e) => e.preventDefault()}>
           <div className={S.titleBox}>
             <img src={livro} alt="" />
             <h3>Informações do livro</h3>
           </div>
           <div className={S.inputBox}>
-            <input type="text" placeholder="Título" />
+            <input type="text" placeholder="Título" onChange={capturaTitulo} value={titulo} />
 
-            <input type="text" placeholder="Categoria" />
+            <input type="text" placeholder="Categoria" onChange={capturaCategoria} value={categoria} /> 
 
-            <input type="text" placeholder="Autor" />
+            <input type="text" placeholder="Autor" onChange={capturaAutor} value={autor} />
 
-            <input type="text" placeholder="Link da imagem" />
+            <input type="text" placeholder="Link da imagem" onChange={capturaImagem} value={imagem_url} />
           </div>
-          <input className={S.button} type="submit" value="Doar" />
+          <input className={S.button} type="submit" value="Doar" onClick={enviarDados}/>
         </form>
       </section>
     </section>
